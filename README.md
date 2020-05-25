e4cf967701e57366f9a4944a30be1f19df947f49
mayconline <mayconline.ti@gmail.com>
2020-05-25 17:46:52 -0300

    basic structure

---
+++ README.md
@@ -0,0 +1,3 @@
+**GraphQL Apollo Server**
+
+- Testing ...
---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+const resolvers = {
+  Query: {
+    hello: () => 'hello word',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`server started at ${url}`));
