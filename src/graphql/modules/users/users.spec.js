e3ca0df13d75d8944bd5f249a5a7f2b6387a314f
mayconline <mayconline.ti@gmail.com>
2020-05-30 02:19:36 -0300

    add two tests on users.spec

--- src/graphql/modules/users/users.spec.js
+++ src/graphql/modules/users/users.spec.js
@@ -5,8 +5,8 @@ const resolvers = require('../../resolvers');
 
 describe('getUsers', () => {
   const schema = makeExecutableSchema({ typeDefs, resolvers });
-
-  const { query } = mockServer(schema);
+  preserveResolvers = true;
+  const { query } = mockServer(schema, {}, preserveResolvers);
 
   const getUsers = `
     query{
@@ -20,8 +20,34 @@ describe('getUsers', () => {
     }
 `;
 
+  const getUserByEmail = `
+    query{
+      getUserByEmail(email:"digding@dig.com.br"){
+        _id
+        email
+      }
+    }
+`;
+
   it('should return users array', async () => {
     const res = await query(getUsers);
     expect(res).toMatchSnapshot();
+  });
+
+  it('should return one user', async () => {
+    const res = await query(getUserByEmail);
+    expect(res).toMatchSnapshot();
+  });
+
+  it('should return data.getUserByEmail', async () => {
+    const res = await query(getUserByEmail);
+    expect(res).toEqual({
+      data: {
+        getUserByEmail: {
+          _id: '2',
+          email: 'digding@dig.com.br',
+        },
+      },
+    });
   });
 });
