34d24a8e89541a489ccce87d87532d55d9d8cda9
mayconline <mayconline.ti@gmail.com>
2020-06-02 21:24:39 -0300

    change graphql-tools for apollo-server-testing

--- src/graphql/modules/users/users.spec.js
+++ src/graphql/modules/users/users.spec.js
@@ -1,16 +1,20 @@
-const { makeExecutableSchema, mockServer } = require('graphql-tools');
+const { ApolloServer, gql } = require('apollo-server');
+const { createTestClient } = require('apollo-server-testing');
 
 const typeDefs = require('../../typeDefs');
-const resolvers = require('../../resolvers');
+
+const server = new ApolloServer({
+  typeDefs,
+  mocks: true,
+  mockEntireSchema: true,
+});
 
 describe('getUsers', () => {
-  const schema = makeExecutableSchema({ typeDefs, resolvers });
-  preserveResolvers = true;
-  const { query } = mockServer(schema, {}, preserveResolvers);
+  const { query } = createTestClient(server);
 
-  const getUsers = `
-    query{
-      users{
+  const GET_USERS = gql`
+    query getUsers {
+      users {
         _id
         email
         password
@@ -18,36 +22,78 @@ describe('getUsers', () => {
         checkTerms
       }
     }
-`;
+  `;
 
-  const getUserByEmail = `
-    query{
-      getUserByEmail(email:"digding@dig.com.br"){
+  const GET_USER_BY_EMAIL = gql`
+    query getUserByEmail($email: String!) {
+      getUserByEmail(email: $email) {
         _id
         email
       }
     }
-`;
+  `;
 
   it('should return users array', async () => {
-    const res = await query(getUsers);
+    const res = await query({ query: GET_USERS });
     expect(res).toMatchSnapshot();
   });
 
   it('should return one user', async () => {
-    const res = await query(getUserByEmail);
+    const res = await query({
+      query: GET_USER_BY_EMAIL,
+      variables: { email: 'teste@te.com' },
+    });
     expect(res).toMatchSnapshot();
+    expect(res.data).toHaveProperty('getUserByEmail');
   });
 
-  it('should return data.getUserByEmail', async () => {
-    const res = await query(getUserByEmail);
-    expect(res).toEqual({
-      data: {
-        getUserByEmail: {
-          _id: '2',
-          email: 'digding@dig.com.br',
-        },
+  it('should have one _id', async () => {
+    const res = await query({
+      query: GET_USER_BY_EMAIL,
+      variables: { email: 'digding@dig.com.br' },
+    });
+    expect(res.data.getUserByEmail).toHaveProperty('_id');
+  });
+});
+
+describe('create User', () => {
+  const { mutate } = createTestClient(server);
+
+  const CREATE_USER = gql`
+    mutation createUser(
+      $email: String!
+      $password: String!
+      $active: Boolean!
+      $checkTerms: Boolean!
+    ) {
+      createUser(
+        input: {
+          email: $email
+          password: $password
+          active: $active
+          checkTerms: $checkTerms
+        }
+      ) {
+        _id
+        email
+        active
+        checkTerms
+      }
+    }
+  `;
+
+  it('should create user', async () => {
+    const res = await mutate({
+      mutation: CREATE_USER,
+      variables: {
+        email: 'te@te.com.br',
+        password: '123',
+        active: true,
+        checkTerms: true,
       },
     });
+
+    expect(res).toMatchSnapshot();
+    expect(res.data).toHaveProperty('createUser');
   });
 });
