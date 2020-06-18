9d1152055fb7e3122e1bf0a48c69eefccf7bd105
mayconline <mayconline.ti@gmail.com>
2020-06-17 22:19:23 -0300

    create tests for finance

---
+++ src/graphql/modules/finance/finance.spec.js
@@ -0,0 +1,33 @@
+const { server, createTestClient, gql } = require('../../utils/serverMock');
+
+describe('Query Test', () => {
+  const { query } = createTestClient(server);
+
+  const GET_API_FINANCE = gql`
+    query getApiFinance($symbol: String) {
+      getApiFinance(symbol: $symbol) {
+        regularMarketPrice
+        financialCurrency
+        exchange
+        market
+        longName
+        symbol
+      }
+    }
+  `;
+
+  it('should return ticket details', async () => {
+    const res = await query({
+      query: GET_API_FINANCE,
+      variables: { symbol: 'mglu3.sa' },
+    });
+    expect(res).toMatchSnapshot();
+    expect(res.data).toHaveProperty('getApiFinance');
+    expect(res.data.getApiFinance).toHaveProperty('regularMarketPrice');
+    expect(res.data.getApiFinance).toHaveProperty('financialCurrency');
+    expect(res.data.getApiFinance).toHaveProperty('exchange');
+    expect(res.data.getApiFinance).toHaveProperty('market');
+    expect(res.data.getApiFinance).toHaveProperty('longName');
+    expect(res.data.getApiFinance).toHaveProperty('symbol');
+  });
+});
