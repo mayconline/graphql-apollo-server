9f9c1e87926feeaa33edfd85a909b758a993faaf
mayconline <mayconline.ti@gmail.com>
2020-06-22 22:38:21 -0300

    create getCurrentFinanceByTickets

--- src/services/finance.js
+++ src/services/finance.js
@@ -1,28 +1,57 @@
 const api = require('./axios');
 
-module.exports = {
-  getFinance: async ticket => {
-    const res = await api.get(`quote?symbols=${ticket}`);
-    const { result } = await res.data.quoteResponse;
+const fetchApi = async ticket => {
+  const res = await api.get(`quote?symbols=${ticket}`);
+  const { result } = await res.data.quoteResponse;
 
-    const [
-      {
-        regularMarketPrice,
-        financialCurrency,
-        exchange,
-        market,
-        longName,
-        symbol,
-      },
-    ] = result;
-
-    return {
+  const [
+    {
       regularMarketPrice,
       financialCurrency,
       exchange,
       market,
       longName,
       symbol,
-    };
+    },
+  ] = result;
+
+  return {
+    regularMarketPrice,
+    financialCurrency,
+    exchange,
+    market,
+    longName,
+    symbol,
+  };
+};
+
+module.exports = {
+  getFinance: fetchApi,
+  getCurrentFinanceByTickets: async ticketArray => {
+    return await Promise.all(
+      ticketArray.map(
+        async ({ _id, symbol, quantity, averagePrice, grade }) => {
+          const {
+            regularMarketPrice,
+            financialCurrency,
+            exchange,
+            market,
+            longName,
+          } = await fetchApi(symbol);
+          return {
+            _id,
+            symbol,
+            quantity,
+            averagePrice,
+            grade,
+            regularMarketPrice,
+            financialCurrency,
+            exchange,
+            market,
+            longName,
+          };
+        },
+      ),
+    );
   },
 };
