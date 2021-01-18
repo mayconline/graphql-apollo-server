b183aba849c181b9ac816232eb0a715818717b41
mayconline <mayconline.ti@gmail.com>
2021-01-18 12:33:10 -0300

    fixing login

--- src/graphql/modules/authenticate/resolvers.js
+++ src/graphql/modules/authenticate/resolvers.js
@@ -1,12 +1,10 @@
 module.exports = {
-  Query: {
-    login: async (_, args, { dataSources }) =>
-      await dataSources.AuthController.show(args),
-  },
   Mutation: {
     updateRole: async (_, args, { dataSources, hasToken }) =>
       !hasToken
         ? new Error('Token Not Exists')
         : await dataSources.AuthController.update(args, hasToken),
+    login: async (_, args, { dataSources }) =>
+      await dataSources.AuthController.show(args),
   },
 };
