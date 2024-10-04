064f264a3d03f4855af4006998ddca0fd91447e3
mayconline <mayconline.ti@gmail.com>
2024-10-04 18:49:08 -0300

    ci: test deploy vercel 12

--- src/graphql/resolvers.ts
+++ src/graphql/resolvers.ts
@@ -5,23 +5,10 @@ import { env } from '../services/env';
 
 const ext = env.NODE_ENV === 'production' ? 'js' : 'ts';
 
-const isVercel = env.IS_VERCEL === 'TRUE';
-
-console.log(`IS_VERCEL: ${isVercel}`);
-
-console.log(`dir: ${process.cwd()}`);
-console.log(`ext: ${__dirname}`);
-
-const resolversArrayVercel = loadFilesSync(
-  join(process.cwd(), 'modules', '**', `resolvers.ts`),
-);
-
 const resolversArray = loadFilesSync(
   join(__dirname, 'modules', '**', `resolvers.${ext}`),
 );
 
-const resolvers = mergeResolvers(
-  isVercel ? resolversArrayVercel : resolversArray,
-);
+const resolvers = mergeResolvers(resolversArray);
 
 export default resolvers;
--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -5,16 +5,10 @@ import { env } from '../services/env';
 
 const ext = env.NODE_ENV === 'production' ? 'js' : 'ts';
 
-const isVercel = env.IS_VERCEL === 'TRUE';
-
-const typesArrayVercel = loadFilesSync(
-  join(process.cwd(), 'modules', '**', `schema.ts`),
-);
-
 const typesArray = loadFilesSync(
   join(__dirname, 'modules', '**', `schema.${ext}`),
 );
 
-const typeDefs = mergeTypeDefs(isVercel ? typesArrayVercel : typesArray);
+const typeDefs = mergeTypeDefs(typesArray);
 
 export default typeDefs;
