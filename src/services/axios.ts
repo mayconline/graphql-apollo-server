783c1740a9a67b960f611df4ac06ca1ab94e4e42
mayconline <mayconline.ti@gmail.com>
2024-10-04 02:20:25 -0300

    feat: add user agente curl

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -11,12 +11,14 @@ const apiSummary = axios.create({
 
 // Adicionando o header 'User-Agent' nas requisições
 api.interceptors.request.use(config => {
-  config.headers['User-Agent'] = 'curl/7.68.0'; // Adicionando o header
+  config.headers['User-Agent'] =
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)'; // Adicionando o header
   return config;
 });
 
 apiSummary.interceptors.request.use(config => {
-  config.headers['User-Agent'] = 'curl/7.68.0'; // Adicionando o header
+  config.headers['User-Agent'] =
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)'; // Adicionando o header
   return config;
 });
 
