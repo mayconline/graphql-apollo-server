da9df79deff04d916ef717e8abba667f63226900
mayconline <mayconline.ti@gmail.com>
2024-10-04 12:02:47 -0300

    fix: api summary

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -9,10 +9,18 @@ const apiSummary = axios.create({
   baseURL: env.API_SUMMARY,
 });
 
-// Adicionando o header 'User-Agent' nas requisições
+// Adicionando os headers nas requisições
 api.interceptors.request.use(config => {
   config.headers['User-Agent'] =
     'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)'; // Adicionando o header
+  config.headers['Content-Type'] = 'application/json;charset=utf-8'; // Adicionando o Content-Type
+  return config;
+});
+
+apiSummary.interceptors.request.use(config => {
+  config.headers['User-Agent'] =
+    'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.2; .NET CLR 1.0.3705;)'; // Adicionando o header
+  config.headers['Content-Type'] = 'application/json;charset=utf-8'; // Adicionando o Content-Type
   return config;
 });
 
