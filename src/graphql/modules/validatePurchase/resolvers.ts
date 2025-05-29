25d3f5b6f7f31f839b83eb24c09d8aa60395fe4a
mayconline <mayconline.ti@gmail.com>
2025-05-29 15:39:52 -0300

    feat: protect route validate purchase

--- src/graphql/modules/validatePurchase/resolvers.ts
+++ src/graphql/modules/validatePurchase/resolvers.ts
@@ -1,10 +1,12 @@
 export default {
   Mutation: {
-    validatePurchase: async (_, args, { dataSources }) => {
+    validatePurchase: async (_, args, { dataSources, hasToken }) => {
       try {
+        if (!hasToken) throw new Error('Token Not Exists');
+
         return await dataSources.ValidatePurchaseController.validate(args);
       } catch (error: any) {
-        throw new Error(error);
+        throw new Error(error.message);
       }
     },
   },
